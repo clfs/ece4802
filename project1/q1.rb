12f1d7f39ada2812e25235ce20a7e8dd8f7046be
clfs <clfs@users.noreply.github.com>
2016-10-31 11:55:47 -0400

    update

--- project1/q1.rb
+++ project1/q1.rb
@@ -1,9 +1,10 @@
 #!/usr/bin/env ruby
 
+$alphabet = *('A'..'Z')
+
 def break_caesar(pt)
-    alphabet = *('A'..'Z')
     for k in 0..25
-        puts k, pt.tr(alphabet.join, alphabet.rotate(k).join)
+        puts k, pt.tr($alphabet.join, $alphabet.rotate(k).join)
     end
 end
 
