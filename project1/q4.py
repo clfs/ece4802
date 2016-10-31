ff6de79fdeb7e348eede3928307dc50ce2fb2685
clfs <clfs@users.noreply.github.com>
2016-10-31 12:01:55 -0400

    update

--- project1/q4.py
+++ project1/q4.py
@@ -6,8 +6,7 @@ def alphabet_sub(x, y):
     return ALPHABET[(ALPHABET.index(x) - ALPHABET.index(y)) % 26]
 
 def break_autokey2(ct):
-    ct_len = len(ct)
-    for kw_len in range(ct_len):
+    for kw_len in range(len(ct)):
         k = 'A'*kw_len + ct[:-kw_len]
         pt = ''.join(map(alphabet_sub, ct, k))
         print(pt)
