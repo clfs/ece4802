fba7a85fea80b71f9b16cc100649f16ef1bfbf59
clfs <clfs@users.noreply.github.com>
2016-11-15 09:31:37 -0500

    update naming scheme

--- project3/q4.py
+++ project3/q4.py
@@ -15,7 +15,6 @@ with open('Gompei.bmp','rb') as in_file:
     # find size of header
     in_file.seek(10)
     offset = int.from_bytes(in_file.read(4),'little')
-
     # get image header
     in_file.seek(0)
     header = in_file.read(offset)
@@ -24,19 +23,17 @@ with open('Gompei.bmp','rb') as in_file:
     for name, mode in zip(names, modes):
        
         # open new file for writing
-        with open('Gompei_{}.bmp'.format(name),'wb') as out_file:
+        with open('Gompei-{}.bmp'.format(name),'wb') as out_file:
 
             # get image content, no header
             in_file.seek(offset)
             content = in_file.read()
-
             # encrypt content
             if mode == AES.MODE_CTR:
                 aes = AES.new(key, mode, iv, counter=Counter.new(128))
             else:
                 aes = AES.new(key, mode, iv)
             encrypted_content = aes.encrypt(content)
-            
             # write out header and content
             out_file.write(header)
             out_file.write(encrypted_content)
