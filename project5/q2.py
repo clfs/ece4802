3828585591158aea2780f4e9161ad9df8b46eb60
clfs <clfs@users.noreply.github.com>
2016-11-22 20:38:41 -0500

    solved problem 2

---
+++ project5/q2.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+def wrapper(a, b, c, d):
+    print('{:>12} {:>12} {:>12} {:>12}'.format(a, b, c, d))
+    return
+
+def modexp(b, e, m):
+    wrapper('x', 'b', 'e', 'm')
+    x = 1
+    while e > 0:
+        wrapper(x, b, e, m)
+        if e % 2: x = (x * b) % m
+        b = (b * b) % m
+        e >>= 1
+    return x
+
+def main():
+    tests = [{'a': 235973, 'e': 456789, 'p': 583903},
+             {'a': 984327457683, 'e': 2153489582, 'p': 994348472629}]
+    for t in tests:
+        a, e, p = t['a'], t['e'], t['p']
+        assert modexp(a, e, p) == pow(a, e, p)
+    print("Success!")
+    return
+
+if __name__ == '__main__':
+    main()
